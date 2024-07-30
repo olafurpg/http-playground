8e38e4b71d269f0f1d70c02eab95e81e64e0a09f
Olafur Geirsson <olafurpg@gmail.com>
2024-07-30 11:08:00 +0200

    WIP

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const response = await openai.chat.completions.create({
+  model: "gpt-4o-mini",
+  messages: [
+    {
+      role: "user",
+      content: [
+        {
+          type: "text",
+          text: 'Respond with "yes" and nothing else',
+        },
+      ],
+    }
+  ],
+  temperature: 1,
+  max_tokens: 256,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+});
